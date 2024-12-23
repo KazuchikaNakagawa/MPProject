6d5c29259c87e87e49e03ac69296145dfbb883bb
kazuki-bit <yuki-hamu7@keio.jp>
2024-12-23 13:44:21 +0900

    syusei

--- soft/terada.c
+++ soft/terada.c
@@ -6,24 +6,23 @@ unsigned char lcd_vbuf[64][96];
 
 void interrupt_handler(){}
 
-void lcd_set_vbuf_pixel(int row, int col, int r, int g, int b) {
-	r >>= 5; g >>= 5; b >>= 6;
-	lcd_vbuf[row][col] = ((r << 5) | (g << 2) | (b << 0)) & 0xff;
+void lcd_wait(int n) {
+	for (int i = 0; i < n; i++);
+}
+void lcd_cmd(unsigned char cmd) {
+	volatile int *lcd_ptr = (int *)0xff0c;
+	*lcd_ptr = cmd;
+	lcd_wait(1000);
 }
 void lcd_data(unsigned char data) {
 	volatile int *lcd_ptr = (int *)0xff0c;
 	*lcd_ptr = 0x100 | data;
 	lcd_wait(200);
 }
-void lcd_clear_vbuf() {
-	for (int row = 0; row < 64; row++)
-		for (int col = 0; col < 96; col++)
-			lcd_vbuf[row][col] = 0;
-}
-void lcd_sync_vbuf() {
-	for (int row = 0; row < 64; row++)
-		for (int col = 0; col < 96; col++)
-			lcd_data(lcd_vbuf[row][col]);
+void lcd_pwr_on() {
+	volatile int *lcd_ptr = (int *)0xff0c;
+	*lcd_ptr = 0x200;
+	lcd_wait(700000);
 }
 void lcd_init() {
 	lcd_pwr_on();   /* Display power ON */
@@ -37,6 +36,21 @@ void lcd_init() {
 	lcd_cmd(63);
 	lcd_cmd(0xaf);  /* Display ON */
 }
+void lcd_set_vbuf_pixel(int row, int col, int r, int g, int b) {
+	r >>= 5; g >>= 5; b >>= 6;
+	lcd_vbuf[row][col] = ((r << 5) | (g << 2) | (b << 0)) & 0xff;
+}
+void lcd_clear_vbuf() {
+	for (int row = 0; row < 64; row++)
+		for (int col = 0; col < 96; col++)
+			lcd_vbuf[row][col] = 0;
+}
+void lcd_sync_vbuf() {
+	for (int row = 0; row < 64; row++)
+		for (int col = 0; col < 96; col++)
+			lcd_data(lcd_vbuf[row][col]);
+}
+
 void lcd_putc_16(int y, int x, int c) {
     int v2, h2;
 
