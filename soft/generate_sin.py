06af776ca5c37418e9081936619a6ae02eb5510a
KazuchikaNakagawa <KazuchikaNakagawa@gmail.com>
2024-12-23 12:05:14 +0900

    [ADD] sin utilities

---
+++ soft/generate_sin.py
@@ -0,0 +1,20 @@
+from math import sin
+from math import pi
+
+def generate_tan():
+    f = open("sin_table.h", "w")
+    f.write("#ifndef SIN_TABLE_H\n")
+    f.write("#define SIN_TABLE_H\n\n")
+    f.write("double sin_table[256] = {\n")
+    for i in range(0, 256):
+        f.write(str(sin((i - 128) * 2 * pi / 256)))
+        if i != 255:
+            f.write(", \n")
+        else:
+            f.write("\n")
+    f.write("};\n\n")
+
+    f.write("#endif\n")
+    f.close()
+
+generate_tan()
