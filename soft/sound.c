93e5811207ac979bda5174f958372e5f2fbed12d
kazuki-bit <yuki-hamu7@keio.jp>
2024-12-23 17:36:26 +0900

    x

--- soft/sound.c
+++ soft/sound.c
@@ -1,5 +1,5 @@
 void main() {
-    volatile int *iob_ptr = (int *)0xff14;
+    volatile int *iob_ptr = (int *)0xff1c;
     *iob_ptr = 1; lcd_wait(7000000);
     *iob_ptr = 2; lcd_wait(7000000);
     *iob_ptr = 3; lcd_wait(7000000);
